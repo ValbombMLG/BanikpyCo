db67568c4a87baba11e8b277169efd070a26c376
ValbombMLG <47860964+ValbombMLG@users.noreply.github.com>
2025-11-21 20:31:05 +0100

    Add files via upload

---
+++ BanikpyCo.h
@@ -0,0 +1,63 @@
+#ifndef BANIKPYCO_H
+#define BANIKPYCO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+
+// -------------------
+// Basic Types
+// -------------------
+#define bo main
+#define cyslo int
+#define cyslo_desetinne float
+#define dubl double
+#define chachar char
+#define bul bool
+#define prazdno void
+#define hepyco *
+
+// -------------------
+// Constants
+// -------------------
+#define fajne true
+#define nyt false
+#define chuj NULL
+
+// -------------------
+// Control Structures
+// -------------------
+#define kaj if
+#define boinak else
+#define za for
+#define rubat while
+#define zrob do
+#define banik switch
+#define pyco case
+#define fcb default
+#define dlabat break
+#define zdubat continue
+#define beznekam goto
+
+// -------------------
+// Functions
+// -------------------
+#define vitky void
+#define jebat return
+#define mluv printf
+#define posluchaj scanf
+#define uhni malloc
+#define dikes free
+#define uhnipyco calloc
+#define uhnivicpyco realloc
+#define estevicmatikypico sqrt
+#define matikavole pow
+
+// -------------------
+// Symbols
+// -------------------
+#define pyco ;
+
+#endif
